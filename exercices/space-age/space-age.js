87fcd4ed168516bd4b9f7b68d2188a422670d3ed
Snehan Gnanassorian <pr46eml@eduvaud.ch>
2025-11-28 14:09:06 +0100

    feat(space-age) : tests réussis

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -2,7 +2,32 @@
 // This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const EARTH_YEARS_SECONDS = 31557600;
 
-export const age = () => {
-  return 
+const ORBITAL_PERIOD = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+export const age = (planet, seconds) => {
+  period = ORBITAL_PERIOD[planet]
+
+  if(!period == true)
+  {
+    throw new Error('not a planet')
+  }
+
+  
+  let earth_age = seconds / EARTH_YEARS_SECONDS;
+  let age_converted = earth_age / period;
+
+  return parseFloat(age_converted.toFixed(2));
+
+
 };
