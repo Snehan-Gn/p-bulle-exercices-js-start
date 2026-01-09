fc04d39be11cc668b98fbd959e13b640ffe01320
Snehan-Gn <pr46eml@eduvaud.ch>
2026-01-09 13:03:46 +0100

    feat(bank-account):corrections

--- exercices/bank-account/bank-account.js
+++ exercices/bank-account/bank-account.js
@@ -4,36 +4,39 @@
 //
 
 export class BankAccount {
+  #balance
+  #isOpen
+
   constructor() {
-    this._balance = 0;
-    this._isOpen = false;
+    this.#balance = 0;
+    this.#isOpen = false;
   }
 
   open() {
-    if (this._isOpen) throw new ValueError();
-    this._isOpen = true;
-    this._balance = 0;
+    if (this.#isOpen) throw new ValueError();
+    this.#isOpen = true;
+    this.#balance = 0;
   }
 
   close() {
-    if (!this._isOpen) throw new ValueError();
-    this._isOpen = false;
-    this._balance = 0;
+    if (!this.#isOpen) throw new ValueError();
+    this.#isOpen = false;
+    this.#balance = 0;
   }
 
   deposit(amount) {
-    if (!this._isOpen || amount <= 0) throw new ValueError();
-    this._balance += amount;
+    if (!this.#isOpen || amount <= 0) throw new ValueError();
+    this.#balance += amount;
   }
 
   withdraw(amount) {
-    if (!this._isOpen || amount <= 0 || amount > this._balance) throw new ValueError();
-    this._balance -= amount;
+    if (!this.#isOpen || amount <= 0 || amount > this.#balance) throw new ValueError();
+    this.#balance -= amount;
   }
 
   get balance() {
-    if (!this._isOpen) throw new ValueError();
-    return this._balance;
+    if (!this.#isOpen) throw new ValueError();
+    return this.#balance;
   }
 }
 
